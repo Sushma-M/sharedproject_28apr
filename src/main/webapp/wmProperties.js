2230299ce8de27ae756d1b632d7268153e283de9
root <root@118c0eb1b20c>
2016-04-28 06:00:54 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,10 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "defaultLanguage" : "en",
+  "displayName" : "SharedProject_28Apr",
+  "homePage" : "Main",
+  "name" : "sharedproject_28apr",
+  "platformType" : "WEB",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
