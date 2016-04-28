a48e15bf709b5bf70d186a105fb0ef7e6b616c7f
Surya null <surya.talluri+6@wavemaker.com>
2016-04-28 06:22:33 +0000

    New files added - [ObjectFactory.java,Multiply.java,AddResponse.java,Add.java,Calculator.java,package-info.java,CalculatorService.java,Calculator.spring.xml,CalculatorSoap.java,Divide.java,servicedef.xml,CalculatorSoap_CalculatorSoap_Client.java,Calculator.settings.json,CalculatorSoap_CalculatorSoap12_Client.java,CalculatorController.java,DivideResponse.java,SubtractResponse.java,MultiplyResponse.java,Calculator.wsdl,Subtract.java] Files modified - [Main.html,project-services.xml,page.min.html,app.variables.json] Refactored files - [] Fixed bugs - [] Removed files - []

---
+++ services/Calculator/src/com/sharedproject_28apr/services/calculator/package-info.java
@@ -0,0 +1,6 @@
+/*Copyright (c) 2016-2017 gmail.com All Rights Reserved.
+ This software is the confidential and proprietary information of gmail.com You shall not disclose such Confidential Information and shall use it only in accordance
+ with the terms of the source code license agreement you entered into with gmail.com*/
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://tempuri.org/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.sharedproject_28apr.services.calculator;
